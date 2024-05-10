24a8793b3c3a2a4b71c6515d30659523ba07fcdd
Powers <49866401+DonPangPang@users.noreply.github.com>
2024-05-11 03:06:48 +0800

    temp

--- Controllers/StreamingController.cs
+++ Controllers/StreamingController.cs
@@ -1,6 +1,5 @@
 using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc;
 using System.IO;
 using System.IO.Pipelines;
 using System.Threading.Tasks;
@@ -15,21 +14,19 @@ namespace MediaServerDemo.Controllers
     [ApiController]
     public class StreamingController : ControllerBase
     {
-        //private static readonly ConcurrentDictionary<string, Channel<byte[]>> Channels = new ConcurrentDictionary<string, Channel<byte[]>>();
+        private static readonly ConcurrentDictionary<string, Channel<ReadOnlyMemory<byte>>> Channels = new ConcurrentDictionary<string, Channel<ReadOnlyMemory<byte>>>();
 
-        private static readonly ConcurrentDictionary<string, WeakReference<Channel<byte[]>>> Channels = new ConcurrentDictionary<string, WeakReference<Channel<byte[]>>>();
 
-        private static readonly VideoStreamManager Manager = new VideoStreamManager();
+        private  static readonly VideoStreamManager Manager = new VideoStreamManager();
 
         [HttpPost("{id}")]
         [RequestSizeLimit(100_000_000)] // Limit the request size to 10MB
         public async Task<IActionResult> Post(string id)
         {
-            if (!Channels.TryGetValue(id, out var weakReference) || !weakReference.TryGetTarget(out var channel) || channel.Writer.TryWrite([]))
+            if (!Channels.TryGetValue(id, out var channel) || !channel.Writer.TryWrite(new ReadOnlyMemory<byte>([])))
             {
-                channel = Channel.CreateUnbounded<byte[]>();
-
-                Channels[id] = new WeakReference<Channel<byte[]>>(channel);
+                channel = Channel.CreateUnbounded<ReadOnlyMemory<byte>>();
+                Channels[id] = channel;
             }
             var data = ArrayPool<byte>.Shared.Rent(8192);
             try
@@ -37,7 +34,7 @@ namespace MediaServerDemo.Controllers
                 int bytesRead;
                 while ((bytesRead = await Request.Body.ReadAsync(data.AsMemory(0, data.Length))) > 0)
                 {
-                    var chunk = data.AsSpan(0, bytesRead).ToArray();
+                    var chunk = data.AsMemory(0, bytesRead).ToArray();
                     await channel.Writer.WriteAsync(chunk);
                 }
             }
@@ -47,35 +44,12 @@ namespace MediaServerDemo.Controllers
             }
             channel.Writer.Complete();
             return Ok();
-
-            //var channel = Manager.GetVideoStream(id);
-            //if ( channel is null || !channel.Writer.TryWrite([]))
-            //{
-            //    channel = Channel.CreateUnbounded<byte[]>();
-            //    Manager.AddVideoStream(id, channel);
-            //}
-            //var data = ArrayPool<byte>.Shared.Rent(8192);
-            //try
-            //{
-            //    int bytesRead;
-            //    while ((bytesRead = await Request.Body.ReadAsync(data.AsMemory(0, data.Length))) > 0)
-            //    {
-            //        var chunk = data.AsSpan(0, bytesRead).ToArray();
-            //        await channel.Writer.WriteAsync(chunk);
-            //    }
-            //}
-            //finally
-            //{
-            //    ArrayPool<byte>.Shared.Return(data);
-            //}
-            //channel.Writer.Complete();
-            //return Ok();
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(string id)
         {
-            if (Channels.TryGetValue(id, out var weakReference) && weakReference.TryGetTarget(out var channel))
+            if (Channels.TryGetValue(id, out var channel))
             {
                 return new PushStreamResult(async outputStream =>
                 {
@@ -84,18 +58,18 @@ namespace MediaServerDemo.Controllers
                     {
                         await foreach (var buffer in channel.Reader.ReadAllAsync(cts.Token))
                         {
-                            await outputStream.WriteAsync(buffer.AsMemory(0, buffer.Length));
+                            var c = buffer.Slice(0, buffer.Length);
+                            await outputStream.WriteAsync(c);
                             await outputStream.FlushAsync();
                             cts.CancelAfter(TimeSpan.FromSeconds(10)); // Reset the cancellation token to cancel after 10 seconds
                         }
                     }
                     catch (OperationCanceledException)
                     {
-                        //Channels.TryRemove(id, out _);
+                        Channels.TryRemove(id, out _);
                     }
                     finally
                     {
-                        Channels.TryRemove(id, out _);
                         GC.Collect();
                         GC.WaitForPendingFinalizers();
                     }
@@ -105,36 +79,7 @@ namespace MediaServerDemo.Controllers
             {
                 return NotFound();
             }
-            //var channel = Manager.GetVideoStream(id);
-            //if (channel is not null)
-            //{
-            //    return new PushStreamResult(async outputStream =>
-            //    {
-            //        var cts = new CancellationTokenSource();
-            //        try
-            //        {
-            //            await foreach (var buffer in channel.Reader.ReadAllAsync(cts.Token))
-            //            {
-            //                await outputStream.WriteAsync(buffer.AsMemory(0, buffer.Length));
-            //                await outputStream.FlushAsync();
-            //                cts.CancelAfter(TimeSpan.FromSeconds(10)); // Reset the cancellation token to cancel after 10 seconds
-            //            }
-            //        }
-            //        catch (OperationCanceledException)
-            //        {
-            //            Manager.CancelVideoStream(id);
-            //        }
-            //        finally
-            //        {
-            //            GC.Collect();
-            //            GC.WaitForPendingFinalizers();
-            //        }
-            //    }, "video/x-flv");
-            //}
-            //else
-            //{
-            //    return NotFound();
-            //}
+            
         }
 
         public class PushStreamResult : IActionResult
